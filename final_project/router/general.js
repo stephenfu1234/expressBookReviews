5ac4597d138bb6862e219fdf5618195f75db7725
Stephen Fu <stephenfu1@gmail.com>
2023-08-21 04:45:43 -0400

    updated files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,7 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get the book list available in the shop
@@ -19,25 +19,42 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  for (book in books) {
+    if(book.author == author) {
+        return res.send(book)
+    }
+  }
+
+  return res.status(300).json({message: "Author not found"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  for (book in books) {
+    if(book.title == title) {
+        return res.send(book)
+    }
+  }
+
+  return res.status(300).json({message: "Title not found"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
