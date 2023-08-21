ee1b66307b6f6aba0ca6dfa44561b3f00b7136b5
Stephen Fu <stephenfu1@gmail.com>
2023-08-21 10:53:13 -0400

    latest

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,19 +1,44 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  //Write your code here  
+  const username = req.body.username;
+  const password = req.body.password;
+
+console.log(password)
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
@@ -29,8 +54,8 @@ public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
 
   for (book in books) {
-    if(book.author == author) {
-        return res.send(book)
+    if(books[book].author == author) {
+        return res.send(books[book])
     }
   }
 
@@ -43,8 +68,8 @@ public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
 
   for (book in books) {
-    if(book.title == title) {
-        return res.send(book)
+    if(books[book].title == title) {
+        return res.send(books[book])
     }
   }
 
@@ -54,6 +79,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn
   res.send(books[isbn].reviews)
 });
 
